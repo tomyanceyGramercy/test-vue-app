ff01387054da6cf4acb4b6ae88dd8542918a8b97
tomyanceyGramercy <65623232+tomyanceyGramercy@users.noreply.github.com>
2024-10-23 20:04:13 -0700

    test-commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -11,7 +11,7 @@
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  heyo
+  <HelloWorld />
 </template>
 
 <style scoped>
